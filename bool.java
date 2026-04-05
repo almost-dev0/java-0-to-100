74b11da19065859eaac9ec9b01f484202cd1bb4f
Omkar <omkarcodes6108@gmail.com>
2026-04-06 02:00:55 +0530

    This program explores the use of boolean data type

---
+++ bool.java
@@ -0,0 +1,12 @@
+public class bool 
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int b = 6;
+        
+        boolean c = (a == b);
+
+        System.out.println(c);
+    }
+}

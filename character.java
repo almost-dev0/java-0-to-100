8ad8ee3178d455e6a73c5e27773ae2596b367e86
Omkar <omkarcodes6108@gmail.com>
2026-04-06 01:59:30 +0530

    This program explores the use of character data type

---
+++ character.java
@@ -0,0 +1,9 @@
+public class character 
+{
+    public static void main(String[] args) 
+    {
+        //In JAVA character occupies 2 Bytes
+        char c = 'a';
+        System.out.println(c);    
+    }
+}

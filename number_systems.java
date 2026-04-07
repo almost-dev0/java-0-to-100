7f8b62e9dc9c31b511b1917ea44af88f6628128e
Omkar <omkarcodes6108@gmail.com>
2026-04-08 00:00:20 +0530

    Added hexadecimal

--- number_systems.java
+++ number_systems.java
@@ -1,6 +1,5 @@
 /*
-    this programs explores how we can print numbers from various
-    number systems like binary, hexadecima, to decimal
+    this programs explores how we can print numbers from various number systems like binary, hexadecima, to decimal
 */
 
 public class number_systems 
@@ -10,6 +9,9 @@ public class number_systems
         // binary
         int num1 = 0b1001;
         System.out.println(num1);
+
+        int num2 = 0x8a;
+        System.out.println(num2);
     }
     
 }

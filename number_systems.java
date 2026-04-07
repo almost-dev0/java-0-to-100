403af524a7855d8288c88831793c3c8738c2ccc8
Omkar <omkarcodes6108@gmail.com>
2026-04-07 23:53:26 +0530

    this programs explores how we can print numbers from various number systems like binary, hexadecimal,etc to decimal

---
+++ number_systems.java
@@ -0,0 +1,15 @@
+/*
+    this programs explores how we can print numbers from various
+    number systems like binary, hexadecima, to decimal
+*/
+
+public class number_systems 
+{
+    public static void main(String[] args) 
+    {
+        // binary
+        int num1 = 0b1001;
+        System.out.println(num1);
+    }
+    
+}

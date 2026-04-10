136212a3b22d18cc4474c21f89a819e842ea6684
Omkar <omkarcodes6108@gmail.com>
2026-04-11 00:03:56 +0530

    minor modification

--- odd.java
+++ odd.java
@@ -1,15 +1,14 @@
-public public class even {
+public public class odd {
     public static void main(String[] args) 
     {
         System.out.println("Even numbers from 1 to 100:");
-        for (int i = 1; i <= 100; i++) 
+            for (int i = 1; i <= 100; i++) 
             {
-                if (i % 2 != 0) {
-                System.out.print(i + " ");
+                if (i % 2 != 0) 
+                {
+                    System.out.print(i + " ");
+                }
             }
-        }
     }
 }
- {
-    
-}
+

9c1264b66a4bf4f72a9cbea3cf1f7b3ed31e9c1a
Omkar <omkarcodes6108@gmail.com>
2026-04-21 17:13:44 +0530

    This program explores the calculation of area of circle through hardcoded radius value

---
+++ areaofcircle_hardcoded.java
@@ -0,0 +1,11 @@
+//This program explores the calculation of area of circle through hardcoded radius value
+
+public class areaofcircle_hardcoded 
+{
+    public static void main(String[] args) 
+    {
+        double r = 10;
+        double a = 3.142 * r * r ;
+        System.out.println("Area of circle is : "+a);    
+    }    
+}
